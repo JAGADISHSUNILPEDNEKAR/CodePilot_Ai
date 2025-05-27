fd0058c5737dcad3424066e7d39fc9745a5a4c37
rishab938 <rishabjeevan37@gamil.com>
2025-05-28 01:09:14 +0530

    highe

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,1 +1,16 @@
-// TODO: Implement this file
+const express=require('express');
+const aiRoutes=require('./routes/ai.routes');
+const cors=require('cors');
+
+const app=express();
+
+app.use(express.json());
+app.use(cors());
+app.get('/',(req,res)=>{
+  res.send('Hello World!')
+
+})
+
+app.use('/ai',aiRoutes);
+module.exports=app;
+// TODO: implement controller logic
