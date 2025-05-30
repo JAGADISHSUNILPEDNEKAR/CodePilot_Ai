5ed251d9001b2dd60c8f4d16954ef3e9eb147626
Arpit Maurya <arpitbabu802@gmail.com>
2025-05-30 16:57:19 +0530

    added update auth.routes  routes

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login, profile } = require('../controllers/auth.controller');
+const { signup, login, profile, updateProfile } = require('../controllers/auth.controller');
 const auth = require('../middleware/auth');
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/user/profile', auth, profile);
+router.put('/user/updateProfile', auth, updateProfile);
 
 module.exports = router; 
